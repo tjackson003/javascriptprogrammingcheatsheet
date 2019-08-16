0480fca9863c619744b44ff44124766c23c0c630
TJ Jackson <elitefourfresh@gmail.com>
2019-08-15 23:52:34 -0400

    first commit

---
+++ jsPractice.js
@@ -0,0 +1,88 @@
+/* WELCOME TO THIS JAVASCRIPT PRACTICE PAGE */
+
+/********** BASIC JAVASCRIPT **********/
+
+
+
+/********** BASIC JAVASCRIPT - NUMBERS **********/
+
+
+
+/********** BASIC JAVASCRIPT - STRINGS **********/
+
+
+
+/********** BASIC JAVASCRIPT - WORD BLANKS **********/
+
+
+
+/********** BASIC JAVASCRIPT - ARRAYS **********/
+
+
+
+/********** BASIC JAVASCRIPT - SHOPPING LIST **********/
+
+
+
+/********** BASIC JAVASCRIPT - FUNCTIONS **********/
+
+
+
+/********** BASIC JAVASCRIPT - STAND IN LINE **********/
+
+
+
+/********** BASIC JAVASCRIPT - BOOLEANS **********/
+
+
+
+/********** BASIC JAVASCRIPT - IF, ELSE IF, & ELSE **********/
+
+
+
+/********** BASIC JAVASCRIPT - GOLF CODE **********/
+
+
+
+/********** BASIC JAVASCRIPT - SWITCHES **********/
+
+
+
+/********** BASIC JAVASCRIPT - MORE ON RETURN FUNCTIONS **********/
+
+
+
+/********** BASIC JAVASCRIPT - COUNTING CARDS **********/
+
+
+
+/********** BASIC JAVASCRIPT - OBJECTS **********/
+
+
+
+/********** BASIC JAVASCRIPT - RECORD COLLECTION **********/
+
+
+
+/********** BASIC JAVASCRIPT - LOOPS **********/
+
+
+
+/********** BASIC JAVASCRIPT - PROFILE LOOKUP **********/
+
+
+
+/********** BASIC JAVASCRIPT - GENERATE RANDOM NUMBERS **********/
+
+
+
+/********** BASIC JAVASCRIPT - PARSEINT **********/
+
+
+
+/********** BASIC JAVASCRIPT - TERNARY OPERATOR **********/
+
+
+
+/********** BASIC JAVASCRIPT - ECMASCRIPT 6 **********/
+
